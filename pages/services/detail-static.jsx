c1e893479412999422c30f5a589e2406e566150a
Aditya Fabio <fabioaditya1995@gmail.com>
2022-03-08 10:31:53 +0700

    service detail static fix

--- pages/services/detail-static.jsx
+++ pages/services/detail-static.jsx
@@ -20,24 +20,24 @@ export default function services() {
             <div class="z-0 grid grid-cols-1 h-[650px] bg-cover mt-[-100px]" style={{backgroundImage: `url('https://www.thespruce.com/thmb/NuFOlvnclSdD92adneqIHjhgBC8=/700x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/remove-salt-stains-from-shoes-clothes-2147178-01-5616c76b4c2941f0963e00d83e2e16e5.jpg')`}}> 
                 <div className='z-1 w-[100vw] h-[650px] bg-[#000009] bg-opacity-30 text-center'>
                     <div class="z-2 grid grid-cols-2 gap-4 bg-cover mt-[100px]">
-                        {/* Desc Card */}
-                        <div className='ml-[15vh] mt-[10vh] z-3 w-[75vh] h-[60vh] bg-[#ffffff] bg-opacity-60 text-center rounded-lg'>
-                            <p className="text-black bold text-3xl mt-[2vh]">
+                        {/* Desc Card */}git
+                        <div className='ml-[15vh] mt-[10vh] z-3 w-[75vh] h-[60vh] bg-[#ffffff] bg-opacity-60 hover:bg-opacity-80 text-left rounded-lg'>
+                            <p className="text-black bold text-4xl mt-[3.5vh] ml-[5vh]">
                                 Regular Cleaning  
                             </p>
-                            <p className="text-black text-xl mt-[10vh]"> 
-                                Descritption 
+                            <p className="text-black text-xl mt-[7.5vh] ml-[5vh] w-[65vh]"> 
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim feugiat ut montes, diam malesuada auctor nunc. Aliquam habitant nulla rhoncus sapien.  
                             </p>
-                            <h1 className="bottom-0 right-0"> Service may varry depend on shoes material<bintang className="text-red-600">*</bintang></h1>
+                            <h1 className="text-md italic ml-[5vh] mt-[15vh]"> Service may varry depend on shoes material<bintang className="text-red-600">*</bintang></h1>
                         </div>
                         {/* Desc Card End*/}
                         {/* Price and Button*/}
-                        <div class="object-bottom">
-                            <p className="text-white text-5xl mt-[60vh] mb-[1vh]"> 
+                        <div class="z-4 object-bottom ml-[5vh]">
+                            <p className="z-5 text-white text-6xl hover:text-7xl mt-[55vh] mb-[3vh]"> 
                                 Rp.25.0000
                             </p>
-                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-6 border border-blue-700 rounded">
-                                <p className="text-white text-xl"> Book a Service </p>
+                            <button class="bg-[#175C8C] hover:bg-white text-white hover:text-black font-bold py-4 px-6 border border-black rounded-lg">
+                                <p className="text-xl rounded-xl"> Book a service </p>
                             </button>
                         </div>
                         {/* Price and Button end*/}
@@ -47,7 +47,6 @@ export default function services() {
             <div class="grid grid-cols-1 mt-[-30px]">
                 <Service />
             </div>
-            
         </section>
     );
 }
