2343838f840c6e5d43a79c8ea4646ec0d6f9da0c
Aditya Fabio <fabioaditya1995@gmail.com>
2022-03-08 18:06:54 +0700

    static form fix

--- pages/form-payment.jsx
+++ pages/form-payment.jsx
@@ -23,13 +23,13 @@ export default function formpayment() {
                                 <p className="text-black bold text-xl">
                                     Service type: Regular Cleaning  
                                 </p>
-                                <p className="text-black text-sm"> 
+                                <p className="text-black text-sm hover:text-lg hover:text-gray-600 "> 
                                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dignissim feugiat ut montes, diam malesuada auctor nunc. Aliquam habitant nulla rhoncus sapien.  Lorem ipsum dolor sit amet, consectetur adipiscing elit.  
                                 </p>
                             </div>
                             {/*form Section */}
                             {/*quantity */}
-                            <div className="grid grid-cols-3 mt-[0.5vh]">
+                            <div className="grid grid-cols-3">
                                 <h1 className="text-left text-[#175C8C] bold text-lg ml-[3vw]">
                                     Quantity<dot className="text-red-600">*</dot>
                                 </h1>
@@ -50,7 +50,7 @@ export default function formpayment() {
                                     Phone Number<dot className="text-red-600">*</dot>
                                 </h1>
                             </div>
-                            <div className="grid grid-cols-3 mt-[0.25vh] ml-[5vh]">
+                            <div className="grid grid-cols-3 ml-[5vh]">
                                 <div className={style.input}>
                                     <input type="Name" id='Name' name='Name' placeholder='Payment Method' required />
                                     <label>Payment Method</label>
@@ -61,8 +61,8 @@ export default function formpayment() {
                                 </div>
                             </div>
                             {/*payment-phone number end */}
-                            {/*city - pickup date - Subtotal*/}
-                            <div className="grid grid-cols-3 mt-[0.25vh]">
+                            {/*city - pickup date*/}
+                            <div className="grid grid-cols-3">
                                 <h1 className="text-left text-[#175C8C] bold text-lg ml-[3vw]">
                                     City<dot className="text-red-600">*</dot>
                                 </h1>
@@ -70,38 +70,38 @@ export default function formpayment() {
                                     Pick-Up Date<dot className="text-red-600">*</dot>
                                 </h1>
                             </div>
-                            <div className="grid grid-cols-3 mt-[0.25vh] ml-[5vh]">
+                            <div className="grid grid-cols-3 ml-[5vh]">
                                 <div className={style.input}>
                                     <input type="Name" id='Name' name='Name' placeholder='City' required />
-                                    <label>Name</label>
+                                    <label>City</label>
                                 </div>
                                 <div className={style.input}>
                                     <input type="Email" id='email' name='Email' placeholder='Pick-Up Date' required />
-                                    <label>E-mail</label>
+                                    <label>Pick-Up Date</label>
                                 </div>
                             </div>
                             {/*city - pickup date */}
-                            {/*Adress */}
-                            <div className="grid grid-cols-2 mt-[0.25vh]">
+                            {/*Adress - Subtotal*/}
+                            <div className="grid grid-cols-2">
                                 <h1 className="text-left text-[#175C8C] bold text-lg ml-[3vw]">
                                     Adress <dot className="text-red-600">*</dot>
                                 </h1>
                             </div>
-                            <div className="grid grid-cols-2 mt-[0.25vh] ml-[5vh]">
+                            <div className="grid grid-cols-2 ml-[5vh]">
                                 <div className={style.input2}>
-                                    <input type="Name" id='Name' name='Name' placeholder='Adress' required />
+                                    <input class="" type="Name" id='Name' name='Name' placeholder='Adress' required />
                                 </div>
                                     <div className="ml-[10vw] mt-[-6.5vh] mb-[1vh]">
                                     <div className="">
-                                        <p className="text-2xl text-center bold ">Subtotal</p>
-                                        <h1 className="text-2xl text-center bold ">Rp.25.000</h1>
+                                        <p className="text-2xl text-center bold hover:text-3xl ">Subtotal</p>
+                                        <h1 className="text-2xl text-center bold hover:text-3xl ">Rp.25.000</h1>
                                     </div>
                                     <button class="bg-[#175C8C] hover:bg-white text-white hover:text-black font-bold py-2 px-3 border border-black rounded-lg mt-[1vh] ml-[2vw]">
                                         <p className="text-md text-center rounded-xl"> Confirm Order </p>
                                     </button>
                                 </div>
                             </div>
-                            {/*Adress end*/}
+                            {/*Adress - subtotal end*/}
                             {/*form Section end */}
                         </div>
                         {/* Desc Card End*/}
