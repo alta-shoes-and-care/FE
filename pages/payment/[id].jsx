3f0658ea8f2798129419e1fc9f05f4853ed2fe06
Aditya Fabio <fabioaditya1995@gmail.com>
2022-03-10 22:34:33 +0700

    fixing bug

--- pages/payment/[id].jsx
+++ pages/payment/[id].jsx
@@ -47,57 +47,107 @@ export default function formpayment(props) {
                                 Confirm your order
                             </p>
                         </div>
+
                         {/* Desc Card */}
-                        <div className='my-auto ml-[25vw] z-3 w-[50vw] h-[70vh] bg-[#ffffff] bg-opacity-60 hover:bg-opacity-80 text-left rounded-lg'>
+
+                        <div className='my-auto ml-[25vw] z-3 w-[50vw] h-[70vh] bg-[#ffffff] bg-opacity-90 text-left rounded-lg'>
                             <div className="grid grid-cols-1 text-center mt-[1vh]  px-10 py-2">
-                                <p className="text-black bold text-xl">
+                                <p className="text-black bold text-2xl">
                                     Service type: {services.title}
                                 </p>
-                                <p className="text-black text-sm mt-[1vh]"> 
+                                <p className="text-black text-md mt-[1vh]"> 
                                     {services.description}
                                 </p>
                             </div>
+
                             {/*form Section */}
+
                             {/*patment - phone number - quantity */}
+
                             <div className="grid grid-cols-3 mt-[1vh]">
                                 <h1 className="text-left text-[#175C8C] bold text-lg ml-[3vw]">
                                     Payment Method<dot className="text-red-600">*</dot>
                                 </h1>
-                                <h1 className="text-left text-[#175C8C] bold text-lg ml-[2vw]">
+                                <h1 className="text-left text-[#175C8C] bold text-lg">
                                     Phone Number<dot className="text-red-600">*</dot>
                                 </h1>
-                                <h1 className="ml-[2.5vh] text-center text-[#175C8C] bold text-lg">
+                                <h1 className="text-center text-[#175C8C] bold text-lg">
                                     Quantity<dot className="text-red-600">*</dot>
                                 </h1>
                             </div>
-                            <div className="grid grid-cols-3 ml-[5vh]">
+
+                            <div className="grid grid-cols-3 ml-[0.5vw]">
+
                                 <div className={style.input}>
-                                    <select>
+                                    <select className="text-gray-500">
                                         <option selected disabled>
                                             Choose Payment
                                         </option>
+                                        <option>
+
+                                        </option>
                                     </select>
                                 </div>
-                                <div className={style.input}>
-                                    <input type="text" id='text' name='text' placeholder='Phone Number' required />
+
+                                <div className="">
+                                    <label htmlFor="phonenumber" className="sr-only">
+                                        Phone number
+                                    </label>
+                                    <input
+                                        id="phonenumber"
+                                        name="phonenumber"
+                                        type="phonenumber"
+                                        maxLength="3"
+                                        placeholder="Phone Number"
+                                        autoComplete="off"
+                                        required
+                                        className="h-[30px] bg-transparent appearance-none relative block w-full px-3 py-2 border-2 border-primary placeholder-gray-500 text-gray-500 md:text-[18px] rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:z-10 sm:text-sm"
+                                        value={""}
+                                        onChange={(e) => {
+                                        setPhonenumber(e.target.value);
+                                        }}
+                                    />
                                 </div>
-                                <div className={style.input3}>
-                                    <input className="text-center" type="number" id='number' name='Number' min='1' placeholder='1' required />
+
+                                 <div className="">
+                                    <label htmlFor="quantity" className="sr-only">
+                                        Quantity
+                                    </label>
+                                    <input
+                                        id="quantity"
+                                        name="quantity"
+                                        type="number"
+                                        maxLength="3"
+                                        placeholder="1"
+                                        autoComplete="off"
+                                        required
+                                        className="mx-auto h-[30px] bg-transparent appearance-none relative block w-[5vw] px-3 py-2 border-2 border-primary placeholder-gray-500 text-gray-500 md:text-[18px] rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:z-10 sm:text-sm"
+                                        value={""}
+                                        min="1"
+                                        onChange={(e) => {
+                                        setQuantity(e.target.value);
+                                        }}
+                                    />
                                 </div>
+
                             </div>
+
                             {/*payment-phone number end */}
+
                             {/*city - pickup date*/}
+
                             <div className="grid grid-cols-3">
                                 <h1 className="text-left text-[#175C8C] bold text-lg ml-[3vw]">
                                     City<dot className="text-red-600">*</dot>
                                 </h1>
-                                <h1 className="text-left text-[#175C8C] bold text-lg ml-[2vw]">
+                                <h1 className="text-left text-[#175C8C] bold text-lg ">
                                     Pick-Up Date<dot className="text-red-600">*</dot>
                                 </h1>
                             </div>
-                            <div className="grid grid-cols-3 ml-[5vh]">
+
+                            <div className="grid grid-cols-3 ">
                                 <div className={style.input}>
-                                    <select>
+                                    <select className="text-gray-500">
                                         <option selected disabled>
                                             Choose City
                                         </option>
@@ -117,40 +167,93 @@ export default function formpayment(props) {
                                         <option>Yogyakarta</option>
                                     </select>
                                 </div>
-                                <div className={style.input}>
-                                    <input type="date" id='date' name='date' placeholder='Pick-Up Date' required />
+
+                                <div className="">
+                                    <label htmlFor="pickupdate" className="sr-only">
+                                        Pick up date
+                                    </label>
+                                    <input
+                                        id="pickupdate"
+                                        name="pickupdate"
+                                        type="date"
+                                        maxLength="3"
+                                        placeholder="Pick Up Date"
+                                        autoComplete="off"
+                                        required
+                                        className="ml-[0.3vw] h-[30px] bg-transparent appearance-none relative block w-full px-3 py-2 border-2 border-primary placeholder-gray-500 text-gray-500 md:text-[18px] rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:z-10 sm:text-sm"
+                                        value={""}
+                                        onChange={(e) => {
+                                        setPhonenumber(e.target.value);
+                                        }}
+                                    />
                                 </div>
+
                             </div>
+
                             {/*city - pickup date */}
+
                             {/*Adress - Subtotal*/}
+
                             <div className="grid grid-cols-2">
                                 <h1 className="text-left text-[#175C8C] bold text-lg ml-[3vw]">
-                                    Adress <dot className="text-red-600">*</dot>
+                                    Address <dot className="text-red-600">*</dot>
                                 </h1>
                             </div>
-                            <div className="grid grid-cols-2 ml-[5vh]">
-                                <div className={style.input2}>
-                                    <input class="" type="Adress" id='Adress' name='Adress' placeholder='Adress' required />
+
+                            <div className="grid grid-cols-2 ml-[6vh]">
+
+                                <div className="">
+                                    <label htmlFor="Address" className="sr-only">
+                                        Address
+                                    </label>
+                                    <textarea
+                                        id="Address"
+                                        name="Adress"
+                                        type="text"
+                                        maxLength="3"
+                                        placeholder="Adress"
+                                        autoComplete="off"
+                                        required
+                                        className="h-[20vh] bg-transparent appearance-none relative block w-[30.5vw] px-3 py-2 border-2 border-primary placeholder-gray-500 text-gray-500 md:text-[18px] rounded-lg focus:outline-none focus:ring-primary focus:border-primary focus:z-10 sm:text-sm"
+                                        value={""}
+                                        onChange={(e) => {
+                                        setAdress(e.target.value);
+                                        }}
+                                    />
                                 </div>
-                                <div className="ml-[10vw]">
-                                    <div className="ml-[-1vh]">
+
+                                <div className="ml-[7vw]">
+                                    <div className="">
                                         <p className="text-2xl text-center bold">Subtotal</p>
                                         <h1 className="text-2xl text-center bold">
                                             <NumberFormat value={services.price} displayType={'text'} decimalSeparator={','} thousandSeparator={'.'} prefix={'Rp'} />,00
                                         </h1>
                                     </div>
-                                    <button class="bg-[#175C8C] hover:bg-white text-white hover:text-black font-bold py-2 px-3 border border-black rounded-lg mt-[1vh] ml-[2vw]">
+                                    <button className="ml-[2.5vw] bg-primary hover:bg-white text-white hover:text-primary font-bold py-2 px-3 border-2 border-primary rounded-lg mt-[1vh]">
                                         <p className="text-md text-center rounded-xl"> Confirm Order </p>
                                     </button>
                                 </div>
+                                
                             </div>
+
                             {/*Adress - subtotal end*/}
-                            {/*form Section end */}
+
+                            {/*form Section end */} 
+
                         </div>
+
                         {/* Desc Card End*/}
+
                     </div>
                 </div>
             </div>
+
+            <style jsx global>{`
+                select option {
+                    background-opacity:0;
+                }
+            `}</style>
+
         </section>
     );
 }
