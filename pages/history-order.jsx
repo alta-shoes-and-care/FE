815575694f48fb253d266a299bb78f7f8875ab6e
unknown <myusufuntung@gmail.com>
2022-03-13 23:02:40 +0700

    fix bug

--- pages/history-order.jsx
+++ pages/history-order.jsx
@@ -3,7 +3,11 @@ import styles from "../styles/History.module.css";
 import { FaMoneyBillAlt } from "react-icons/fa";
 import { FcCalendar } from "react-icons/fc";
 import { RiMessage2Line } from "react-icons/ri";
-import { AiOutlineNumber, AiOutlineFileDone } from "react-icons/ai";
+import {
+  AiOutlineNumber,
+  AiOutlineFileDone,
+  AiOutlineShoppingCart,
+} from "react-icons/ai";
 import Swal from "sweetalert2";
 import { useRouter } from "next/router";
 import NumberFormat from "react-number-format";
@@ -15,8 +19,14 @@ function History() {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
   const [history, sethistory] = useState([[]]);
+  console.log(history);
 
   useEffect(() => {
+    if (typeof window !== "undefined") {
+      if (!localStorage.getItem("token")) {
+        router.push("/");
+      }
+    }
     setLoading(true);
     const token = localStorage.getItem("token");
     const config = {
@@ -26,7 +36,7 @@ function History() {
       .get(`https://ynwahid.cloud.okteto.net/orders/me`, config)
       .then(({ data }) => {
         sethistory(data.data);
-        console.log(data.data);
+        console.log(data.data, "masuk");
       })
       .catch((err) => {
         console.log(err.response, "error");
@@ -65,7 +75,24 @@ function History() {
             }, 2000);
           })
           .catch((err) => {
-            console.log(err.response, "error");
+            if (err.response.status === 401) {
+              Swal.fire({
+                title: "Your session has ended!",
+                text: "Please login again to continue.",
+                icon: "error",
+                showCancelButton: false,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Ok",
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  router.push("/login");
+                  localStorage.clear();
+                }
+              });
+            } else {
+              Swal.fire("Ooppss!", "Sorry, the server is error.", "error");
+            }
           })
           .finally(() => {
             setLoading(false);
@@ -80,67 +107,77 @@ function History() {
   return (
     <div className={`flex justify-center items-center ${styles.historybg}`}>
       <div
-        className={` rounded-xl w-[700px] h-screen overflow-y-scroll my-8 p-4 flex flex-col items-center backdrop-blur-[10px] bg-[#ffffff88]`}
+        className={` rounded-xl w-[800px] h-screen overflow-y-scroll my-8 p-4 flex flex-col items-center backdrop-blur-[10px] bg-[#ffffff88]`}
       >
         <h1 className=" text-3xl text-center  font-bold my-3 ">
           History Order
         </h1>
 
         {/* card */}
-        <div className=" w-[700px] overflow-y-scroll flex flex-col items-center">
-          {history.map((el, i) => (
-            <div
-              className={` w-[550px] rounded-lg py-2 px-3 my-3 bg-white shadow-md`}
-            >
-              <h1 className=" text-xl">{el.service_title}</h1>
-              <div className=" bg-gray-600 w-[200px] my-1 h-0.5"></div>
-              {/* status */}
-              <div className=" flex mt-1 justify-between">
-                <div className=" flex">
-                  <p className=" text-green-600 text-xl mt-0.5 mr-1">
-                    <FaMoneyBillAlt />
-                  </p>
-                  <p>
-                    Rp.{" "}
-                    <NumberFormat
-                      value={el.total}
-                      displayType={"text"}
-                      thousandSeparator={true}
-                    />
-                  </p>
-                </div>
-                <div className=" flex">
-                  <p className="text-xl mt-0.5 mr-1">
-                    <FcCalendar />
-                  </p>
-                  <p>{moment(el.date).format("Do MMMM YYYY")}</p>
-                </div>
-                <div className=" flex">
-                  <p className="text-xl mt-0.5 mr-1">
-                    <RiMessage2Line />
-                  </p>
-                  <p>{el.status}</p>
-                </div>
-                <div className=" flex">
-                  <p className="text-xl mt-0.5 mr-0.5">
-                    <AiOutlineNumber />
-                  </p>
-                  <p>{el.id}</p>
-                </div>
-                {el.status === "delivering" ? (
-                  <div className=" flex hover:text-primary">
-                    <p className="text-xl mt-0.5 mr-0.5">
-                      <AiOutlineFileDone />
+        {!history[0].id ? (
+          <div></div>
+        ) : (
+          <div className=" w-[700px] overflow-y-scroll flex flex-col items-center">
+            {history.map((el, i) => (
+              <div
+                className={` w-[630px] rounded-lg py-2 px-3 my-3 bg-white shadow-md`}
+              >
+                <h1 className=" text-xl">{el.service_title}</h1>
+                <div className=" bg-gray-600 w-[200px] my-1 h-0.5"></div>
+                {/* status */}
+                <div className=" flex mt-1 justify-between">
+                  <div className=" flex">
+                    <p className=" text-green-600 text-xl mt-0.5 mr-1">
+                      <FaMoneyBillAlt />
                     </p>
-                    <button onClick={() => handleConfirm(el)}>Confirm</button>
+                    <p>
+                      Rp.{" "}
+                      <NumberFormat
+                        value={el.total}
+                        displayType={"text"}
+                        thousandSeparator={true}
+                      />
+                    </p>
                   </div>
-                ) : (
-                  <div></div>
-                )}
+                  <div className=" flex">
+                    <p className="text-xl mt-0.5 mr-1">
+                      <FcCalendar />
+                    </p>
+                    <p>{moment(el.date).format("D MMMM YYYY")}</p>
+                  </div>
+                  <div className=" flex">
+                    <p className="text-xl mt-0.5 mr-1">
+                      <RiMessage2Line />
+                    </p>
+                    <p>{el.status}</p>
+                  </div>
+                  <div className=" flex">
+                    <p className="text-xl mt-0.5 mr-1">
+                      <AiOutlineShoppingCart />
+                    </p>
+                    <p>{el.is_paid ? "Paid" : "Not Paid"}</p>
+                  </div>
+                  <div className=" flex">
+                    <p className="text-xl mt-0.5 mr-0.5">
+                      <AiOutlineNumber />
+                    </p>
+                    <p>{el.id}</p>
+                  </div>
+                  {el.status === "delivering" ? (
+                    <div className=" flex hover:text-primary">
+                      <p className="text-xl mt-0.5 mr-0.5">
+                        <AiOutlineFileDone />
+                      </p>
+                      <button onClick={() => handleConfirm(el)}>Confirm</button>
+                    </div>
+                  ) : (
+                    <div></div>
+                  )}
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
